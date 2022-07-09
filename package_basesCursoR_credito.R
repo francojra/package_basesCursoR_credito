3bb2cfecf7a28c552fa212821ea525c371db2e42
Jeanne Franco <44760301+francojra@users.noreply.github.com>
2022-07-09 18:48:08 -0300

    Add files via upload

--- package_basesCursoR_credito.R
+++ package_basesCursoR_credito.R
@@ -62,3 +62,33 @@ p2 <- ggplot(cred3, aes(x = fct_reorder(trabalho, med), y = med)) +
                     ymax = med + se), width = 0.2, size = 0.9) +
   labs(x = "Trabalho", y = "Despesas")
 p2
+
+cred4 <- cred1 %>%
+  group_by(estado_civil) %>%
+  summarise(med = mean(renda, na.rm = T),
+            sd = sd(renda, na.rm = T),n = n(),
+            se = sd/sqrt(n)) %>%
+  drop_na()
+View(cred4)
+
+p3 <- ggplot(cred4, aes(x = fct_reorder(estado_civil, med), y = med)) +
+  geom_col(fill = "#377eb8", color = "black") +
+  geom_errorbar(aes(x = estado_civil, y = med, ymin = med - se,
+                    ymax = med + se), width = 0.2, size = 0.9) +
+  labs(x = "Estado Civil", y = "Renda")
+p3
+
+cred5 <- cred1 %>%
+  group_by(trabalho) %>%
+  summarise(med = mean(renda),
+            sd = sd(renda),n = n(),
+            se = sd/sqrt(n)) %>%
+  drop_na()
+View(cred5)
+
+p4 <- ggplot(cred5, aes(x = fct_reorder(trabalho, med), y = med)) +
+  geom_col(fill = "#ff7f00", color = "black") +
+  geom_errorbar(aes(x = trabalho, y = med, ymin = med - se,
+                    ymax = med + se), width = 0.2, size = 0.9) +
+  labs(x = "Trabalho", y = "Renda")
+p4
